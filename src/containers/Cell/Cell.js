61cdf84ca7321b5529cbe69c71b23a8f6215482f
Ioannis Papatsoris <giannhs95@yahoo.gr>
2018-08-22 17:54:45 +0300

    Add number colors

--- src/containers/Cell/Cell.js
+++ src/containers/Cell/Cell.js
@@ -13,8 +13,14 @@ const Cell = (props) => {
 
     const stylesArray = [];
     stylesArray.push(styles.Cell);
-    if (props.clicked && !props.number) {
-        stylesArray.push(styles.Clicked);
+    if (props.clicked || props.revealed) {
+        switch (props.number) {
+            case 0: stylesArray.push(styles.Clicked); break;
+            case 1: stylesArray.push(styles.NumberOne); break;
+            case 2: stylesArray.push(styles.NumberTwo); break;
+            case 3: stylesArray.push(styles.NumberThree); break;
+            default: stylesArray.push(styles.NumberFour); break;
+        }
     }
 
     if (props.revealed) {
